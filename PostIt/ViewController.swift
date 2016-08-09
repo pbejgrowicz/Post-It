1edacff2d8ec25f1e23b02a683a41358acc05373
Patryk Bejgrowicz <pbejgrowicz@speednet.pl>
2016-08-09 09:28:03 +0200

    Initial Commit

---
+++ PostIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostIt
+//
+//  Created by Speednet on 09.08.2016.
+//  Copyright © 2016 Patryk Bejgrowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
